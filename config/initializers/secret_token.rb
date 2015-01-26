a93901f694ad938fd1664bafc01fde0d5388beb0
Ubuntu64 <ubuntu64@ubuntu.(none)>
2015-01-25 19:46:33 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogg::Application.config.secret_key_base = 'ee3f1b885e06f7227f1046f84c8362deaedf819b0cc4edfa2d70ec2474b3f8b500488b46640bd23adb5248f469f1acd7b10a1611ff7c0819ce9224b113dc0dc2'
